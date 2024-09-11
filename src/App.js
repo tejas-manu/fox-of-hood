821b5fcfae55484daa7ba1fea7c8d2fc46ef4c39
tejas-manu <tejasmanu.s07@gmail.com>
2024-09-11 19:35:56 -0400

    Trial

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+// import { BrowserRouter as Router } from 'react-router-dom';
+import Title from "./components/Title";
 
+function App () {
+    return(
+        // <Router>
+            <Title />
+        // </Router>
 
+    )
+}
 
+export default App;
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,2 +1,18 @@
 import React from "react";
-import {} from './Title_style'
+import {Head, HeadContainer, HeadOption} from './Title_style'
+
+const Title = () =>{
+    return (
+        <Head>
+            <HeadContainer>
+                Fox of Hood
+            </HeadContainer>
+            
+            <HeadOption>
+                Menu
+            </HeadOption>
+        </Head>
+    )
+}
+
+export default Title
